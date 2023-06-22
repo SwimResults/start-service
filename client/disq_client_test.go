01dbe9fb864c7b4a096231185cd7c9164a8bdc38
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-06-22 20:11:37 +0200

    add unused client tests

---
+++ client/disq_client_test.go
@@ -0,0 +1,29 @@
+package client
+
+//import (
+//	"fmt"
+//	"github.com/swimresults/start-service/model"
+//	"testing"
+//	"time"
+//)
+//
+//func TestDisqualificationClient_ImportDisqualification(t *testing.T) {
+//	client := NewDisqualificationClient("http://localhost:8087/")
+//
+//	start1 := model.Start{
+//		Meeting: "IESC19",
+//		Event:   5,
+//		//AthleteName:     "Simon Meier",
+//		//AthleteYear:     1234,
+//		//AthleteTeamName: "blub Team",
+//		Lane:       3,
+//		HeatNumber: 11,
+//	}
+//
+//	r, _, e := client.ImportDisqualification(start1, "Guter Grund", time.Now())
+//	if e != nil {
+//		fmt.Printf(e.Error())
+//	}
+//	fmt.Println(r)
+//	//fmt.Printf("id: %s, number: %d, start: %s", r.Identifier.String(), r.Number, r.StartEstimation.Format("15:04"))
+//}
---
+++ client/start_client_test.go
@@ -0,0 +1,59 @@
+package client
+
+//
+//import (
+//	"fmt"
+//	"github.com/swimresults/start-service/model"
+//	"testing"
+//	"time"
+//)
+//
+//func TestStartClient_ImportStart(t *testing.T) {
+//	client := NewStartClient("http://localhost:8087/")
+//
+//	start1 := model.Start{
+//		Meeting: "IESC19",
+//		Event:   5,
+//		//AthleteName:     "Simon Meier",
+//		//AthleteYear:     1234,
+//		//AthleteTeamName: "blub Team",
+//		Lane:       3,
+//		HeatNumber: 11,
+//		Rank:       2,
+//		Certified:  true,
+//	}
+//
+//	r, _, e := client.ImportStart(start1)
+//	if e != nil {
+//		fmt.Printf(e.Error())
+//	}
+//	fmt.Println(r)
+//	//fmt.Printf("id: %s, number: %d, start: %s", r.Identifier.String(), r.Number, r.StartEstimation.Format("15:04"))
+//}
+//
+//func TestStartClient_ImportResult(t *testing.T) {
+//	client := NewStartClient("http://localhost:8087/")
+//
+//	start1 := model.Start{
+//		Meeting: "IESC19",
+//		Event:   5,
+//		//AthleteName:     "Simon Meier",
+//		//AthleteYear:     1234,
+//		//AthleteTeamName: "blub Team",
+//		Lane:       3,
+//		HeatNumber: 11,
+//	}
+//
+//	result1 := model.Result{
+//		Time:       23100000,
+//		ResultType: "final",
+//		AddedAt:    time.Time{},
+//	}
+//
+//	r, _, e := client.ImportResult(start1, result1)
+//	if e != nil {
+//		fmt.Printf(e.Error())
+//	}
+//	fmt.Println(r)
+//	fmt.Printf("type: %s, time: %f", r.ResultType, r.Time.Seconds())
+//}
