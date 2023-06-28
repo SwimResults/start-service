2ce164c08e46041bbf82fdd2d10493bfc7f2ba9c
konrad2002 <konrad@schwimmteamerzgebirge.de>
2023-06-28 21:40:16 +0200

    result type changes

--- model/result.go
+++ model/result.go
@@ -6,6 +6,6 @@ import (
 
 type Result struct {
 	Time       time.Duration `json:"time,omitempty" bson:"time,omitempty"`               // PDF + DSV + Livetiming
-	ResultType string        `json:"result_type,omitempty" bson:"result_type,omitempty"` // registration; livetiming_result; result; reaction;
+	ResultType string        `json:"result_type,omitempty" bson:"result_type,omitempty"` // registration; livetiming_result; result_list; reaction;
 	AddedAt    time.Time     `json:"added_at,omitempty" bson:"added_at,omitempty"`       // automatically
 }
