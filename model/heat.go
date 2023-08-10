2cb79de77ef8ace07b3bd8cbc173b61289fdc531
konrad2002 <konrad@schwimmteamerzgebirge.de>
2023-08-10 12:34:43 +0200

    heat changes

--- model/heat.go
+++ model/heat.go
@@ -6,11 +6,12 @@ import (
 )
 
 type Heat struct {
-	Identifier      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`                           // automatically
-	Meeting         string             `json:"meeting,omitempty" bson:"meeting,omitempty"`                   // automatically
-	Event           int                `json:"event,omitempty" bson:"event,omitempty"`                       // PDF + DSV
-	Number          int                `json:"number,omitempty" bson:"number,omitempty"`                     // PDF + DSV
-	StartEstimation time.Time          `json:"start_estimation,omitempty" bson:"start_estimation,omitempty"` // PDF
-	StartAt         time.Time          `json:"start_at,omitempty" bson:"start_at,omitempty"`                 // automatically
-	FinishedAt      time.Time          `json:"finished_at,omitempty" bson:"finished_at,omitempty"`           // automatically
+	Identifier           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`                                       // automatically
+	Meeting              string             `json:"meeting,omitempty" bson:"meeting,omitempty"`                               // automatically
+	Event                int                `json:"event,omitempty" bson:"event,omitempty"`                                   // PDF + DSV
+	Number               int                `json:"number,omitempty" bson:"number,omitempty"`                                 // PDF + DSV
+	StartEstimation      time.Time          `json:"start_estimation,omitempty" bson:"start_estimation,omitempty"`             // PDF				estimation from start list
+	StartDelayEstimation time.Time          `json:"start_delay_estimation,omitempty" bson:"start_delay_estimation,omitempty"` // PDF				estimation from delay
+	StartAt              time.Time          `json:"start_at,omitempty" bson:"start_at,omitempty"`                             // automatically		actual moment were it started
+	FinishedAt           time.Time          `json:"finished_at,omitempty" bson:"finished_at,omitempty"`                       // automatically		actual moment were it finished
 }
