b112797d998f7b56d3c05bab529865aa333497ce
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-10-01 10:48:33 +0300

    result comment for lap times

--- model/result.go
+++ model/result.go
@@ -6,6 +6,6 @@ import (
 
 type Result struct {
 	Time       time.Duration `json:"time,omitempty" bson:"time,omitempty"`               // PDF + DSV + Livetiming
-	ResultType string        `json:"result_type,omitempty" bson:"result_type,omitempty"` // registration; livetiming_result; result_list; reaction;
+	ResultType string        `json:"result_type,omitempty" bson:"result_type,omitempty"` // registration; livetiming_result; result_list; reaction; 50_m_lap; 100_m_lap; 150_m_lap;
 	AddedAt    time.Time     `json:"added_at,omitempty" bson:"added_at,omitempty"`       // automatically
 }
