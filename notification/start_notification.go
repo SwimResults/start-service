4afe7f1c2876ea22a83a5cd93655079075d1d16f
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-12-07 01:21:19 +0100

    print notification errors

--- notification/start_notification.go
+++ notification/start_notification.go
@@ -9,15 +9,23 @@ import (
 func SendStartNotificationForAthlete(start model.Start) {
 	minutes := start.Heat.StartDelayEstimation.Sub(time.Now()).Minutes()
 	fmt.Printf("notifying athlete for event: %d, heat: %d, lane: %d, athlete: %s in %f minutes\n", start.Event, start.HeatNumber, start.Lane, start.AthleteName, minutes)
-	go notificationClient.SendNotificationForMeetingAndAthlete(
-		serviceKey,
-		start.Meeting,
-		start.Athlete,
-		fmt.Sprintf("Wettkampf %d", start.Event),
-		fmt.Sprintf("Du startest in ca. %f Minuten in Lauf %d auf Bahn %d.", minutes, start.HeatNumber, start.Lane),
-		"athlete",
-		"time-sensitive",
-	)
+	go func() {
+		response, err := notificationClient.SendNotificationForMeetingAndAthlete(
+			serviceKey,
+			start.Meeting,
+			start.Athlete,
+			fmt.Sprintf("Wettkampf %d", start.Event),
+			fmt.Sprintf("Du startest in ca. %f Minuten in Lauf %d auf Bahn %d.", minutes, start.HeatNumber, start.Lane),
+			"athlete",
+			"time-sensitive",
+		)
+
+		if err != nil {
+			fmt.Printf("notify failed: %e\n", err)
+		}
+		println(response.Body)
+		println(response.ApnsId)
+	}()
 }
 
 func SendStartNotificationForFavourite(start model.Start) {
