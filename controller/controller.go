bc9d0f3cce99dd5f5490b475dafdb8b20dafc208
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-06-30 16:19:23 +0200

    upgrade go 1.24; add stats endpoint

--- controller/controller.go
+++ controller/controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/semihalev/gin-stats"
 	"github.com/swimresults/start-service/service"
 	"net/http"
 	"os"
@@ -26,6 +27,9 @@ func Run() {
 	registrationController()
 
 	router.GET("/actuator", actuator)
+	router.GET("/stats", func(c *gin.Context) {
+		c.JSON(http.StatusOK, stats.Report())
+	})
 
 	err := router.Run(":" + port)
 	if err != nil {
