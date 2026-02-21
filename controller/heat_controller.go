0544c1cfbcf9fd64a83c8baef886d91c961da845
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2026-02-21 23:30:46 +0100

    add method to get heats by event and meeting

--- controller/heat_controller.go
+++ controller/heat_controller.go
@@ -22,6 +22,7 @@ func heatController() {
 	router.GET("/heat/meet/:meet_id/event_list", getHeatsByMeetingForEventList)
 	router.GET("/heat/meet/:meet_id/info", getHeatInfoByMeeting)
 	router.GET("/heat/meet/:meet_id/event/:event_id/info", getHeatInfoByMeetingAndEvent)
+	router.GET("/heat/meet/:meet_id/event/:event_id", getHeatsByMeetingAndEvent)
 	router.GET("/heat/meet/:meet_id/current", getCurrentHeat)
 	router.GET("/heat/meet/:meet_id/current_next", getCurrentNextHeat)
 
@@ -180,6 +181,29 @@ func getHeatInfoByMeetingAndEvent(c *gin.Context) {
 	}
 
 	c.IndentedJSON(http.StatusOK, info)
+}
+
+func getHeatsByMeetingAndEvent(c *gin.Context) {
+	meeting := c.Param("meet_id")
+
+	if meeting == "" {
+		c.String(http.StatusBadRequest, "no meeting id given")
+		return
+	}
+
+	event, err1 := strconv.Atoi(c.Param("event_id"))
+	if err1 != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "given event_id is not of type number"})
+		return
+	}
+
+	heats, err := service.GetHeatsByMeetingAndEvent(meeting, event)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, heats)
 }
 
 func getCurrentHeat(c *gin.Context) {
