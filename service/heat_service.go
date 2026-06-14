cf4a2e666f9fbed76bce35df66faa05d2aebe207
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2026-06-14 10:40:19 +0200

    fix finish time will not be set if already existing

--- service/heat_service.go
+++ service/heat_service.go
@@ -461,7 +461,7 @@ func ImportHeat(heat model.Heat) (model.Heat, bool, error) {
 		existing.StartAt = heat.StartAt
 		changed = true
 	}
-	if !heat.FinishedAt.IsZero() && !(heat.StartEstimation.Hour() == 0 && heat.StartEstimation.Minute() == 0) {
+	if !heat.FinishedAt.IsZero() {
 		existing.FinishedAt = heat.FinishedAt
 		changed = true
 	}
